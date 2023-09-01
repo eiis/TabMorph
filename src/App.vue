6548a7216f491e70e59059e8cb34ac6d51f5ff8c
zhangdi <1258956799@qq.com>
2023-09-01 23:28:53 +0800

    feat:update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
+import { ref } from 'vue';
+
+const textareaValue = ref(window.location.href); // 定义一个响应式引用存储textarea的值
+function click(){
+  console.log(1111);
+  textareaValue.value ='1111'
+}
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <textarea 
+      v-model="textareaValue" 
+      class="border rounded p-2 w-full h-32" 
+      placeholder="请输入内容">
+    </textarea>
+    <div @click="click">Click</div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
